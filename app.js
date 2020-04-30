33e68e5220d2f86f06a54bf62028a72c82cb6cb0
chavdanimit@gmail.com <chavdanimit@gmail.com>
2020-04-29 20:04:51 -0400

    final

--- app.js
+++ app.js
@@ -210,11 +210,11 @@ var UIController= (function(){
             //create HTML strings with palceholder text
             if(type=== 'inc'){
             element=Domstrings.icomeContainer;    
-            html='<div class="item clearfix" id="inc-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            html='<div class="item clearfix" id="inc-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><ion-icon name="close-outline"></ion-icon></button></div></div></div>';
             }
             else if(type==='exp'){
             element=Domstrings.expensesContainer;    
-            html='<div class="item clearfix" id="exp-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            html='<div class="item clearfix" id="exp-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><ion-icon name="close-outline"></ion-icon></button></div></div></div>';
             }
             
             //replace the placeholder text with some actual data
